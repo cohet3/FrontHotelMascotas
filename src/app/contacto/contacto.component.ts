a4f016b725a95ea0624267a872f4fa30031dbdfb
FaleteRusso <faleterusso89@gmail.com>
2024-03-03 19:37:32 +0100

    3Marzo 19.35 ubicacion y contacto añadido logo creado y guardado en imagenes

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+  correo: string ='';
+  nombre: string ='';
+  telefono: string ='';
+  comentario: string ='';
+enviado:boolean = false;
+mensaje:string = '';
 
+enviarInfo(){
+  this.enviado=true
+this.mensaje='Sus datos han sido enviados correctamente'
+}
 }
--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -10,8 +10,11 @@ import { ServicioService } from '../_servicio/servicio.service';
   styleUrl: './servicios.component.css'
 })
 export class ServiciosComponent implements OnInit {
+
   constructor(private servicio: ServicioService) { }
+  
   servicios: Servicio[] =[];
+
   ngOnInit(): void {
     this.servicio.listar().subscribe(datos=>this.servicios=datos);
   }
