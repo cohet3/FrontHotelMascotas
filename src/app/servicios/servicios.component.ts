03a8d9522d52ab2f7a1c8f8de6cd35320d083d69
cohet3 <tauro.total90@gmail.com>
2024-03-04 16:05:15 +0100

    css de servicios, carga de imagen y descripción añadida, responsive global añadido

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Servicio } from '../_modelo/servicio';
 import { ServicioService } from '../_servicio/servicio.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-servicios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 })
@@ -14,8 +15,12 @@ export class ServiciosComponent implements OnInit {
   servicios: Servicio[] =[];
   ngOnInit(): void {
     this.servicio.listar().subscribe(datos=>this.servicios=datos);
+    
   }
 
+  trackByFn(index: number, item: any): any {
+    return item.idServicio; // Reemplaza 'id' por la propiedad única de tu modelo
+  }
 
 }
 
