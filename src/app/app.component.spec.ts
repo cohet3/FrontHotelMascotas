496f5a89d169e0df3637901ed8e9286e1237d861
FaleteRusso <faleterusso89@gmail.com>
2024-03-07 21:02:27 +0100

    cambios en esilos, navbar, u demas 21.00 7Mar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hotelMascotas' title`, () => {
+  it(`should have the 'Paraíso Pelido' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hotelMascotas');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hotelMascotas');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Paraíso Peludo');
   });
 });
--- src/app/galeria-fotos/galeria-fotos.component.ts
+++ src/app/galeria-fotos/galeria-fotos.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-galeria-fotos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './galeria-fotos.component.html',
   styleUrl: './galeria-fotos.component.css'
 })
 export class GaleriaFotosComponent {
-}
+public imagenes =[
+  {name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+{name:'Social Club',
+  src:"assets/imagenes/crisPerrin1.jpg"
+},
+]
+ }
