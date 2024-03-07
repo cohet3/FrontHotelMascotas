eda5241d0962b95122580359e47237b7617b318b
Cristina <cristinadel13@hotmail.com>
2024-03-07 19:57:06 +0100

    mascota realizada y dada de alta(id cliente fijo porque si no no dejaba dar de alta a la mascota)

---
+++ src/app/alta-reserva/alta-reserva.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-reserva',
+  standalone: true,
+  imports: [],
+  templateUrl: './alta-reserva.component.html',
+  styleUrl: './alta-reserva.component.css'
+})
+export class AltaReservaComponent {
+
+}
